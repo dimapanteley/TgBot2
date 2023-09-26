ccda1af55b56bbaae76e598d717146698156529e
Dima <dimapanteleyvrach@gmail.com>
2023-09-26 14:06:13 +0300

    Новый бот

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,13 +1,27 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.TelegramException;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScope;
+import com.pengrad.telegrambot.model.botcommandscope.BotCommandScopeDefault;
+import com.pengrad.telegrambot.request.SetMyCommands;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 @Configuration
 public class TelegramBotConfiguration {
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Value("${telegram.bot.token}")
     private String token;
